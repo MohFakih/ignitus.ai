6dad18b602ce741792d7c5b94472ed4048d26f59
Mohamad Fakih <mohfakih2000@gmail.com>
2020-11-24 17:14:44 +0200

    OOP-ified the project. Wood destruction added

--- Basic2DSim.py
+++ Basic2DSim.py
@@ -1,4 +1,33 @@
 import numpy as np
+import pygame
+
+
+TYPE_TO_CHAR = {}
+TYPE_TO_CHAR['air']  =' '
+TYPE_TO_CHAR['fire'] ='X'
+TYPE_TO_CHAR['block']='#'
+TYPE_TO_CHAR['wood'] ='W'
+
+
+BLACK = (0, 0, 0)
+RED   = (255, 0, 0)
+BROWN = (150, 100, 50)
+
+
+
+class CellFactory:
+    def __init__(self, parent_world, parent_grid):
+        self.parent_world = parent_world
+        self.parent_grid = parent_grid
+    
+    def createCell(self, coords, block_type):
+        if block_type == 'air':
+            return Cell_AIR(coords, self.parent_world, self.parent_grid)
+        elif block_type == 'fire':
+            return Cell_FIRE(coords, self.parent_world, self.parent_grid)
+        elif block_type == 'wood':
+            return Cell_WOOD(coords, self.parent_world, self.parent_grid)
+
 
 
 class World:
@@ -23,7 +52,8 @@ class World:
         for line in s_list:
             x, y, block_type = line.split()
             x, y = int(x), int(y)
-            self.grid[x][y] = Cell((x, y), self, self.grid, block_type=block_type)
+            cellFactory = CellFactory(self, self.grid)
+            self.grid[x][y] = cellFactory.createCell((x, y), block_type)
 
     def load_grid_from_string(self, s):
         self.load_grid_from_string_list(s.splitlines())
@@ -40,7 +70,7 @@ class World:
             newGrid[i] = [None] * self.W
         return newGrid
 
-    def init_grid(self, default_type='air'):
+    def init_grid(self, block_type='air'):
         """
             Initialize the grid and fill it with default block types
         """
@@ -48,7 +78,8 @@ class World:
 
         for x in range(self.H):
             for y in range(self.W):
-                self.grid[x][y] = Cell((x, y), self, self.grid, default_type)
+                cellFactory = CellFactory(self, self.grid)
+                self.grid[x][y] = cellFactory.createCell((x, y), block_type)
     
     def list_cells(self):
         L = []
@@ -72,48 +103,21 @@ class World:
         for old_cell in self.list_cells():
             x = old_cell.x
             y = old_cell.y
-
-            countVicinitylames = 0
-            for vicinity_cell in old_cell.list_vicinity_cells():
-                if vicinity_cell.block.block_type == 'fire':
-                    countVicinitylames += 1
-
-            foundAdjacentFlammable = False
-            for adjacent_cell in old_cell.list_adjacent_cells():
-                if adjacent_cell.block.block_type == 'wood':
-                    foundAdjacentFlammable = True
-                    break
-            
-            if old_cell.block.block_type == 'fire':
-                # Check for flammable adjacent:
-                if not foundAdjacentFlammable:
-                    newGrid[x][y] = Cell((x, y), self, newGrid, 'air')
-                else:
-                    # Check for flames in the vicinity
-                    if countVicinitylames < 4 and np.random.randn() < 0.2:
-                        newGrid[x][y] = Cell((x, y), self, newGrid, 'air')
-                    else:
-                        newGrid[x][y] = Cell((x, y), self, newGrid, 'fire')
-            elif old_cell.block.block_type == 'air':
-                if foundAdjacentFlammable:
-                    probability_of_fire = 0.04*countVicinitylames
-                    if np.random.randn() < probability_of_fire:
-                        newGrid[x][y] = Cell((x, y), self, newGrid, 'fire')
-                    else:
-                        newGrid[x][y] = Cell((x, y), self, newGrid, 'air')
-                else:
-                    newGrid[x][y] = Cell((x, y), self, newGrid, 'air')
-            else:
-                newGrid[x][y] = Cell((x, y), self, newGrid, old_cell.block.block_type)
+            newGrid[x][y] = old_cell.step(self, newGrid)
         self.grid = newGrid
 
+        for cell in self.list_cells():
+            cell.propagate(self.grid, self)
 
 class Cell:
     def __init__(self, coords, parent_world, parent_grid, block_type='air'):
         self.x, self.y = coords
         self.parent_world = parent_world
         self.parent_grid = parent_grid
-        self.block = Block(self, block_type)
+        self.block_type =  block_type
+        self.foundAdjacentFlammable = False
+        self.countVicinitylames = 0
+        self.color = BLACK
     
     # TODO: ALL OF THIS CAN BE PRECOMPUTED. i.e.: for each cell, have the indices of its vicnity/adjacents precomputed in some structure instead of computing them when needed.
     def list_cells_k_distant(self, k):
@@ -130,25 +134,79 @@ class Cell:
     def list_vicinity_cells(self):
         return self.list_cells_k_distant(k=2)
     
+    def addAdjacentFlammable(self):
+        self.foundAdjacentFlammable = True
+
+    def addVicinityFlame(self):
+        self.countVicinitylames += 1
+
     def serialize(self):
-        return str(self.x) + " " + str(self.y) + " " + self.block.block_type+"\n"
+        return str(self.x) + " " + str(self.y) + " " + self.block_type+"\n"
 
     def __str__(self):
-        return str(self.block)
-
-
-type_to_char = {}
-type_to_char['air']  =' '
-type_to_char['fire'] ='X'
-type_to_char['block']='#'
-type_to_char['wood'] ='W'
-class Block:
-    def __init__(self, occupying_cell, block_type='air'):
-        self.block_type = block_type
-        self.occupying_cell = occupying_cell
+        return TYPE_TO_CHAR[self.block_type]
     
-    def __str__(self):
-        return type_to_char[self.block_type]
+    def step(self, newWorld, newGrid):
+        pass
+
+    def propagate(self, newWorld, newGrid):
+        pass
+    
+    def getRect(self, block_width, block_height):
+        return [self.x * block_width, self.y * block_height, block_width, block_height]
+    
+    def draw(self, screen, block_width, block_height):
+        rect = self.getRect(block_width, block_height)
+        pygame.draw.rect(screen, self.color, rect)
+
+class Cell_FIRE(Cell):
+    def __init__(self, coords, parent_world, parent_grid):
+        super().__init__(coords, parent_world, parent_grid, 'fire')
+        self.color = RED
+    
+    def step(self, newWorld, newGrid):
+        if not self.foundAdjacentFlammable:
+            return Cell_AIR((self.x, self.y), newWorld, newGrid)
+        else:
+            # Check for flames in the vicinity
+            if self.countVicinitylames < 4 and np.random.randn() < 0.2:
+                return Cell_AIR((self.x, self.y), newWorld, newGrid)
+            else:
+                return Cell_FIRE((self.x, self.y), newWorld, newGrid)
+
+    def propagate(self, newWorld, newGrid):
+        for cell in self.list_vicinity_cells():
+            cell.addVicinityFlame()
+
+
+class Cell_WOOD(Cell):
+    def __init__(self, coords, parent_world, parent_grid, hp=100):
+        super().__init__(coords, parent_world, parent_grid, 'wood')
+        self.hp = hp
+        self.color = BROWN
+    
+    def step(self, newWorld, newGrid):
+        damage = self.countVicinitylames
+        newHP = self.hp-damage
+        if newHP <= 0:
+            return Cell_AIR((self.x, self.y), newWorld, newGrid)
+        else:
+            return Cell_WOOD((self.x, self.y), newWorld, newGrid, newHP)
+
+    def propagate(self, newWorld, newGrid):
+        for cell in self.list_adjacent_cells():
+            cell.addAdjacentFlammable()
 
 
 
+class Cell_AIR(Cell):
+    def __init__(self, coords, parent_world, parent_grid):
+        super().__init__(coords, parent_world, parent_grid, 'air')
+        self.color = BLACK
+    
+    def step(self, newWorld, newGrid):
+        if self.foundAdjacentFlammable:
+            probability_of_fire = 0.04*self.countVicinitylames
+            if np.random.randn() < probability_of_fire:
+                return Cell_FIRE((self.x, self.y), newWorld, newGrid)
+        return Cell_AIR((self.x, self.y), newWorld, newGrid)
--- testSimulation.py
+++ testSimulation.py
@@ -10,9 +10,6 @@ pygame.init()
 size = width, height = 320, 320
 block_width = width/world.W
 block_height= height/world.H
-BLACK = (0, 0, 0)
-RED   = (255, 0, 0)
-BROWN = (150, 100, 50)
 screen = pygame.display.set_mode(size)
 while 1:
     for event in pygame.event.get():
@@ -21,16 +18,10 @@ while 1:
         if event.type == pygame.KEYDOWN:
             if pygame.key.get_pressed()[pygame.K_r]:
                 world.step()
+                print(str(world))
 
     screen.fill(BLACK)
     for cell in world.list_cells():
-        rect = [cell.x * block_width, cell.y * block_height, block_width, block_height]
-        block_type = cell.block.block_type
-        if block_type == 'air':
-            pygame.draw.rect(screen, BLACK, rect)
-        elif block_type == 'wood':
-            pygame.draw.rect(screen, BROWN, rect)
-        elif block_type == 'fire':
-            pygame.draw.rect(screen, RED, rect)
+        cell.draw(screen, block_width, block_height)
 
     pygame.display.flip()
